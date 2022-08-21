f4664b1e2d416cc749d5bc0ec959a22d282788a4
Matteo Lobello <matteo.lobello@protonmail.com>
2022-08-21 13:07:03 +0200

    Update trpc.ts

--- packages/app/src/clients/trpc.ts
+++ packages/app/src/clients/trpc.ts
@@ -2,7 +2,7 @@ import type { AppRouter } from "@api/routes/root.router"
 
 import { createReactQueryHooks } from "@trpc/react"
 
-export const BASE_HOST = "https://fcb5-95-239-237-127.eu.ngrok.io"
+export const BASE_HOST = "http://localhost:4444"
 
 export const trpc = createReactQueryHooks<AppRouter>()
 
